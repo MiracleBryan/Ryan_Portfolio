05e7191ff365a92ba07cb83280dce80a2bc3aed4
Miracle <96289980+MiracleBryan@users.noreply.github.com>
2024-06-06 16:24:16 +1000

    fix download resume func

--- my-portfolio/src/component/naviBar.tsx
+++ my-portfolio/src/component/naviBar.tsx
@@ -1,10 +1,21 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import './naviBar.scss';
+import { BlobItem, BlobServiceClient, StorageSharedKeyCredential} from '@azure/storage-blob';
 
 const NavigationBar = () => {
     const [scrolled, setScrolled] = useState<boolean>(false);
     const [activedBtn, setActiveBtn] = useState<string>('Home');
+
+    //Azure blob setting
+    //const connectionString = "https://ryanportfolio1.blob.core.windows.net/";
+    //const containerName = 'portfolio';
+    //const resumeName = "Ryan Wang Resume.pdf"
+    const sasUrl  = encodeURIComponent(process.env.SASURL!)
+
+    //process.env.SASURL
+    
+
     useEffect(() => {
         const onScroll = () => {
             if (window.scrollY > 50) {
@@ -24,7 +35,38 @@ const NavigationBar = () => {
         setTimeout(() => { }, 500)
     }
 
+    const handleDownloadResume = async () => {
+        try {
+            var file = await fetch(sasUrl)
+            var blob = await file.blob()
+            console.log(file.blob);
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'Ryan_Wang_Resume.pdf';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+            //blobFile.push(await response.blobBody?.text());
+        } catch (e) {
+            console.log(e)
+            window.alert("\n                         Function temporarily unavailable-_-\n\n                    Please contact ryan.wang496@gmail.com")
+        }
 
+
+
+
+        // try {
+        //     const res = await fetch(url)
+        //     console.log("Success: " + res.body)
+        // } catch (e) {
+        //     console.log("Failed")
+        // }
+
+
+    }
 
     return (
         <div className="navContainer">
@@ -39,7 +81,7 @@ const NavigationBar = () => {
                     <Link to="/Ryan_Portfolio/projects" className={`navTag ${activedBtn == 'Projects' ? 'actived' : ''}`} onClick={() => { handleNaviClick('Projects') }}>Projects</Link>
                 </div>
                 <div className="downloadFont">
-                    <Link to="/files/RyanWang_Resume.pdf" className="downloadFont" target="_blank" download>Download Resume</Link>
+                    <div className="downloadFont" onClick={handleDownloadResume}>Download Resume</div>
                 </div>
 
 
